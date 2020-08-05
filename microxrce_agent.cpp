5ab66139dfc11d7446dff04fcad795c60e873f5e
Pablo Garrido <pablogs9@gmail.com>
2020-08-05 10:23:46 +0200

    Working state

--- microxrce_agent.cpp
+++ microxrce_agent.cpp
@@ -17,34 +17,34 @@
 #include <uxr/agent/utils/Callbacks.hpp>
 #include <csignal>
 
-void printGUID(const dds::GUID_t& guid) {
-    std::cout <<  "GUID:\n\t entityId.entityKind: " << (int) guid.entityId().entityKind() << "\n";
-    std::cout <<  "\t entityId.m_entityKey: ";
+// void printGUID(const eprosima::fastrtps::rtps::GUID_t& guid) {
+//     std::cout <<  "GUID:\n\t entityId.entityKind: " << (int) guid.entityId().entityKind() << "\n";
+//     std::cout <<  "\t entityId.m_entityKey: ";
 
-    for (size_t i = 0; i < 3; i++){
-        std::cout << (int) guid.entityId().entityKey()[i] << " ";
-    }
+//     for (size_t i = 0; i < 3; i++){
+//         std::cout << (int) guid.entityId().entityKey()[i] << " ";
+//     }
 
-    std::cout <<  "\n\t guidPrefix: ";
+//     std::cout <<  "\n\t guidPrefix: ";
 
-    for (size_t i = 0; i < 12; i++){
-        std::cout << (int) guid.guidPrefix()[i] << " ";
-    }
-    std::cout <<  "\n";
-}
+//     for (size_t i = 0; i < 12; i++){
+//         std::cout << (int) guid.guidPrefix()[i] << " ";
+//     }
+//     std::cout <<  "\n";
+// }
 
-void on_create_callback(const dds::xrce::ObjectKind& kind, const dds::GUID_t& guid) {
-  std::cout <<  "ObjectKind: " <<(int) kind << "\n";
-  printGUID(guid);
-}
+// void on_create_callback(const dds::xrce::ObjectKind& kind, const eprosima::fastrtps::rtps::GUID_t& guid) {
+//   std::cout <<  "ObjectKind: " <<(int) kind << "\n";
+//   printGUID(guid);
+// }
 
 int main(int argc, char** argv)
 {
     eprosima::uxr::UDPv4Agent agent_udp4(8888, eprosima::uxr::Middleware::Kind::FASTDDS);
     agent_udp4.set_verbose_level(6);
 
-    auto * cb_a = on_create_callback;
-    agent_udp4.add_oncreate_callback(cb_a);
+    // auto * cb_a = on_create_callback;
+    // agent_udp4.add_oncreate_callback(cb_a);
     
     agent_udp4.start();
 
